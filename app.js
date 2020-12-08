cd42d112a6e2397301e81d1e1307d67ce0037461
kamil.ponikwia <kamil.ponikwia@gmail.com>
2020-12-08 18:34:36 +0100

    first commit

--- app.js
+++ app.js
@@ -67,4 +67,10 @@ app.use(function(err, req, res, next) {
   res.render('error')
 })
 
-module.exports = app
+//module.exports = app
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+  console.log(`Express server listening on port ${app.get('port')}`);
+})
